ff5a7eaf3a8e6dfe3f39d8a04c1abe265fde8adc
Afsane Fadaei <a.fadaei@irantalent.com>
2020-01-10 22:15:40 +0330

    git ignore updated

---
+++ src/pages/Rxjs/LessonFour.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LessonFour = () => (
+  <div>
+    <h5>Lesson4</h5>
+    <span>Description:</span>
+    <p>
+      sdfjo
+    </p>
+  </div>
+);
+
+export default LessonFour;
--- src/pages/Rxjs/component.js
+++ src/pages/Rxjs/component.js
@@ -2,16 +2,19 @@ import React from 'react';
 import LessonOne from './lessonOne';
 import LessonTwo from './LessonTwo';
 import LessonThree from './LessonThree';
+import LessonFour from './LessonFour';
 
 const MyComponent = () => (
   <div>
     <h4>Learn RXjs</h4>
     <hr />
-    <LessonOne />
+    {/*<LessonOne />*/}
     <hr />
-    <LessonTwo />
+    {/*<LessonTwo />*/}
     <hr />
-    <LessonThree />
+    {/*<LessonThree />*/}
+    <hr />
+    <LessonFour />
     <hr />
   </div>
 );
