607802d1b7fe3fb1f57f61f5c229d7faf5c2b938
Afsane Fadaei <a.fadaei@irantalent.com>
2020-01-10 23:00:24 +0330

    tabs added

--- src/pages/Rxjs/component.js
+++ src/pages/Rxjs/component.js
@@ -1,22 +1,81 @@
 import React from 'react';
-import LessonOne from './lessonOne';
-import LessonTwo from './LessonTwo';
-import LessonThree from './LessonThree';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import LessonFour from './LessonFour';
+import LessonThree from './LessonThree';
+import LessonTwo from './LessonTwo';
+import LessonOne from './lessonOne';
 
-const MyComponent = () => (
-  <div>
-    <h4>Learn RXjs</h4>
-    <hr />
-    {/*<LessonOne />*/}
-    <hr />
-    {/*<LessonTwo />*/}
-    <hr />
-    {/*<LessonThree />*/}
-    <hr />
-    <LessonFour />
-    <hr />
-  </div>
-);
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+const a11yProps = (index: any) => ({
+  id: `simple-tab-${index}`,
+  'aria-controls': `simple-tabpanel-${index}`,
+});
+
+
+const TabPanel = (props: TabPanelProps) => {
+  const {
+    children, value, index, ...other
+  } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+};
+
+const MyComponent = () => {
+  const [value, setValue] = React.useState(3);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <h4>Learn RXjs</h4>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Tab label="Lesson One" {...a11yProps(0)} />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Tab label="Lesson Two" {...a11yProps(1)} />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Tab label="Lesson Three" {...a11yProps(2)} />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Tab label="Lesson Four" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <LessonOne />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <LessonTwo />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <LessonThree />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <LessonFour />
+      </TabPanel>
+    </div>
+  );
+};
 
 export default MyComponent;
